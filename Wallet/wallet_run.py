001b87fcf028c6f982ad886e87d4943cb6f2a398
Jasmin <zasminalave@gmail.com>
2024-06-02 18:01:44 +1000

    add account

---
+++ Wallet/wallet_run.py
@@ -0,0 +1,19 @@
+
+from wallet_ui import *
+from PyQt6.QtWidgets import QWidget, QApplication
+import sys
+class MainWindow(QWidget, Ui_Form):
+    def __init__(self):
+        super().__init__()
+
+        self.setupUi(self)
+
+
+
+if __name__ == "__main__":
+    qApp = QApplication(sys.argv)
+    app = MainWindow()
+    app.show()
+
+    sys.exit(qApp.exec())
+
